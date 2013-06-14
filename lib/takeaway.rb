22f7ce769dbc358883625f7630daf798b5b1834e
Charlotte Cameron <charlottemidgley@gmail.com>
2013-06-14 17:54:16 +0100

    Created dishes hash and 2 tests to check dish item and price.

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,6 +1,16 @@
 class Takeaway
 
-	def initialize
-	end
+	# def initialize(dishes)
+	# end
+
+	DISHES = {
+		"pie" => 3.5,
+		"baked beans" => 2
+	}
+
+	def menu
+		DISHES
+	end	
+
 
 end
--- test/test_takeaway.rb
+++ test/test_takeaway.rb
@@ -1,5 +1,18 @@
-class Test_Takeaway
+require 'minitest/autorun'
+require '../lib/takeaway'
 
-	
+class Test_Takeaway < Minitest::Test
+
+	def setup
+		@takeaway = Takeaway.new
+	end
+
+	def test_hash_contains_dish
+		assert @takeaway.menu.include?("pie")
+	end
+
+	def test_hash_contains_dish_with_price
+		assert_equal 2, @takeaway.menu["baked beans"]
+	end
 
 end
